c4573916ae200b88a6353cffb7c1b112b905c53e
bhavya-sharma-iwell <bhavya.sharma@investwellonline.com>
2024-09-04 18:13:02 +0530

    dlt file

--- adminApp/rootReducer.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from '@reduxjs/toolkit';
-// Import other reducers as needed
-
-const reducers = combineReducers({
-
-  // Add other reducers here
-});
-
-export default reducers
