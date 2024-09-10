b7e665c98ee21c094b8e240705e38d72f77003d4
ishita <ishita.goswami@investwellonline.com>
2024-09-10 14:01:10 +0530

    remove extra code

--- adminApp/features/index.jsx
+++ adminApp/features/index.jsx
@@ -68,36 +68,36 @@ export class Admin extends Component{
               loadThemeCss : false
             })
           }
-    //     if(newProps.loginStore && newProps.loginStore.user && newProps.loginStore.user.userType && !sessionStorage.getItem('localUrl')){
-    //         if(sessionStorage.getItem('previousURLBeforeLogout')){
-    //             let previousURL = sessionStorage.getItem('previousURLBeforeLogout').replace(/"/g,"").split('#')[1]
-    //               sessionStorage.removeItem('previousURLBeforeLogout');
-    //             window.location.href = '#' + previousURL
-    //         }else{
-    //             if(Cookie.getCookie('c_ux')){
-    //                 if(!sessionStorage.getItem('localUrl')){
-    //                     if((window.location.href.split('#')[1].split('/')[1] == 'login' )){
-    //                         window.location.href = '#/admin/ifaLookup'
-    //                     }
-    //                     else{
-    //                         window.location.href = '#'+window.location.href.split('#')[1]
-    //                     }
-    //                 }
-    //             }
-    //         }
-    //         sessionStorage.setItem('localUrl',true)
-    //         sessionStorage.removeItem('previousURLBeforeLogout')
-    //     } 
-    //     if(this.state.updateFabIcon && (this.props.loginStore && this.props.loginStore.user && this.props.loginStore.user.userType) ){
-    //         let favElement = document.getElementById("favicon");
-    //         let path = `${DOCS_LOCATION_DOMAIN}/${INVESTWELL_CLOUD_IMAGES}/${INVESTWELL_LOGOS}/${INVESTWELL_SMALL_LOGOS}/admin_Logo.ico`
-    //         this.setState({ updateFabIcon : false }, CheckImageExists(path,function(existsImage){
-    //           if(existsImage){
-    //             favElement.href=path
-    //           }
-    //         }))
-    //       }  
-    // }
+        // if(newProps.loginStore && newProps.loginStore.user && newProps.loginStore.user.userType && !sessionStorage.getItem('localUrl')){
+        //     if(sessionStorage.getItem('previousURLBeforeLogout')){
+        //         let previousURL = sessionStorage.getItem('previousURLBeforeLogout').replace(/"/g,"").split('#')[1]
+        //           sessionStorage.removeItem('previousURLBeforeLogout');
+        //         window.location.href = '#' + previousURL
+        //     }else{
+        //         if(Cookie.getCookie('c_ux')){
+        //             if(!sessionStorage.getItem('localUrl')){
+        //                 if((window.location.href.split('#')[1].split('/')[1] == 'login' )){
+        //                     window.location.href = '#/admin/ifaLookup'
+        //                 }
+        //                 else{
+        //                     window.location.href = '#'+window.location.href.split('#')[1]
+        //                 }
+        //             }
+        //         }
+        //     }
+        //     sessionStorage.setItem('localUrl',true)
+        //     sessionStorage.removeItem('previousURLBeforeLogout')
+        // } 
+        // if(this.state.updateFabIcon && (this.props.loginStore && this.props.loginStore.user && this.props.loginStore.user.userType) ){
+        //     let favElement = document.getElementById("favicon");
+        //     let path = `${DOCS_LOCATION_DOMAIN}/${INVESTWELL_CLOUD_IMAGES}/${INVESTWELL_LOGOS}/${INVESTWELL_SMALL_LOGOS}/admin_Logo.ico`
+        //     this.setState({ updateFabIcon : false }, CheckImageExists(path,function(existsImage){
+        //       if(existsImage){
+        //         favElement.href=path
+        //       }
+        //     }))
+        //   }  
+    }
     editProfile(){ }
 
     render(){
